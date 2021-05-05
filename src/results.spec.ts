93d2e6cf1267486809e6097f898c0c664236f1d0
tp <arantianpeng@gmail.com>
2021-05-05 17:14:05 +0800

    add test for results

--- src/results.spec.ts
+++ src/results.spec.ts
@@ -1,7 +1,7 @@
 import { resultsCalculator } from './results';
 
 describe('results test', () => {
-  it('should return correct result given status and hand value', () => {
+  it('winner should takes all', () => {
     const pool = new Map([
       ['a', 1],
       ['b', 2],
@@ -19,6 +19,50 @@ describe('results test', () => {
         ['a', 0],
         ['b', 0],
         ['c', 6],
+      ]),
+    );
+  });
+
+  it('multiple winners will share', () => {
+    const pool = new Map([
+      ['a', 3],
+      ['b', 3],
+      ['c', 3],
+    ]);
+    const handValue = new Map([
+      ['a', 2],
+      ['b', 2],
+      ['c', 1],
+    ]);
+
+    const res = resultsCalculator(handValue, pool);
+    expect(res).toEqual(
+      new Map([
+        ['a', 4.5],
+        ['b', 4.5],
+        ['c', 0],
+      ]),
+    );
+  });
+
+  it('winner will only take his bet amount * active player, the rest player will take the rest', () => {
+    const pool = new Map([
+      ['a', 1],
+      ['b', 2],
+      ['c', 3],
+    ]);
+    const handValue = new Map([
+      ['a', 3],
+      ['b', 2],
+      ['c', 1],
+    ]);
+
+    const res = resultsCalculator(handValue, pool);
+    expect(res).toEqual(
+      new Map([
+        ['a', 3],
+        ['b', 2],
+        ['c', 1],
       ]),
     );
   });
